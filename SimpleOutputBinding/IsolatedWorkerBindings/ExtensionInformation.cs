39faaa1a526a9199b6e101704d6efbd017d867e2
Daniël te Winkel <bitbucket@te-winkel.nl>
2022-08-12 10:20:13 +0200

    Add documentation. Add rules to keep line lengths in bounds.

--- SimpleOutputBinding/IsolatedWorkerBindings/ExtensionInformation.cs
+++ SimpleOutputBinding/IsolatedWorkerBindings/ExtensionInformation.cs
@@ -1,3 +1,8 @@
 ﻿using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
 
+/* 
+ * The ExtensionInformationAttribute links the bindings in this assembly to the actual implementation.
+ * This link is a link to a NuGet package with the in-process binding that implement the actual bindings that 
+ * are executed on the host.The NuGet package is referenced by its ID and version.
+ */
 [assembly: ExtensionInformation("SimpleOutputBinding", "1.0.0")]
